d99fcef6500f3407ab998c8f9f2c1af9d43eb757
Lam Tat <81158948+TanLam01@users.noreply.github.com>
2026-08-15 10:29:53 +0700

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const newSet = new Set();
+
+        for(const n of nums) {
+            if(newSet.has(n)) {
+                return true
+            }
+            newSet.add(n);
+        }
+
+        return false;
+    }
+}
